ed8b3444851a7cc6fcc868db1b7764f14775c185
NadiaPironeva <63345028+NadiaPironeva@users.noreply.github.com>
2020-04-10 10:44:38 +0100

    Create FancyBarcodes.java
    Starting second task, second group

---
+++ FancyBarcodes.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class FancyBarcodes {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = Integer.parseInt(scan.nextLine());
+
+    }
+}
